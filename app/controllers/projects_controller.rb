f1f713f394a24ce0eb2d9f8b337886e835657753
jocvegar <jocvegar@gmail.com>
2016-10-26 15:07:09 -0600

    Added edit and destroy project abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,35 +1,52 @@
-class ProjectsController < ApplicationController
-	before_action :find_project, only: [:show, :edit, :update, :destroy]
+	class ProjectsController < ApplicationController
+		before_action :find_project, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@projects = Project.all.order ("created_at desc")
-	end
+		def index
+			@projects = Project.all.order ("created_at desc")
+		end
 
-	def new
-		@project = Project.new
-	end
+		def new
+			@project = Project.new
+		end
 
-	def create
-		@project = Project.new project_params
+		def create
+			@project = Project.new project_params
 
-		if @project.save
-			redirect_to @project, notice: "Guardado, papilon!"
-		else
-			render 'new', notice: "Algo anda malo Joe Matter"
+			if @project.save
+				redirect_to @project, notice: "Guardado, papilon!"
+			else
+				render 'new', notice: "Algo anda malo Joe Matter"
+			end
+		end
+
+		def edit
+		end
+
+		def update
+			if @project.update project_params
+				redirect_to @project, notice: "Updatiado, papilon!"
+			else	
+				render 'edit'
+			end
+		end
+
+		def destroy
+			@project.destroy
+			redirect_to projects_path
+
+		end
+
+		def show	
+		end
+
+		private
+
+		def find_project
+			@project = Project.friendly.find(params[:id])
+		end
+
+		def project_params
+			params.require(:project).permit(:title, :description, :link, :slug)
 		end
 	end
-
-	def show	
-	end
-
-	private
-
-	def find_project
-		@project = Project.friendly.find(params[:id])
-	end
-
-	def project_params
-		params.require(:project).permit(:title, :description, :link, :slug)
-	end
-end
 
